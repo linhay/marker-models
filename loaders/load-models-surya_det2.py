8f2942c1b62b483c4f79ea20b9eb0f05c6119907
linhey <is.linhey@outlook.com>
2024-06-17 18:02:35 +0800

    chore:

--- loaders/load-models-surya_det2.py
+++ loaders/load-models-surya_det2.py
@@ -1,4 +1,4 @@
 from huggingface_hub import hf_hub_download
-hf_hub_download(repo_id="vikp/surya_det2", resume_download=True, filename="config.json")
-hf_hub_download(repo_id="vikp/surya_det2", resume_download=True, filename="model.safetensors")
-hf_hub_download(repo_id="vikp/surya_det2", resume_download=True, filename="preprocessor_config.json")
+hf_hub_download(repo_id="vikp/surya_det2", force_download=True, filename="config.json")
+hf_hub_download(repo_id="vikp/surya_det2", force_download=True, filename="model.safetensors")
+hf_hub_download(repo_id="vikp/surya_det2", force_download=True, filename="preprocessor_config.json")
